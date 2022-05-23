99886083fa094960e1945733a3167519166628d7
LucaseNogueira <lucasenogueira@outlook.com>
2022-05-22 21:58:08 -0300

    Movida a pasta media e adicionado os icones do projeto
    1. - Movido a pasta "media" para fora do pacote "src". A pasta "media" representa todos os arquivos que não representam o código fonte do sistema, mas uma mídia, como icones, txt ou gifs;
    2. - Adicionado todos os icones utilizados neste projeto

--- Trabalho02_DSD/src/main.java
+++ Trabalho02_DSD/src/main.java
@@ -9,7 +9,7 @@ import utils.Leitor;
 public class main {
     
     public static void main(String[] args){
-        File   arquivo = new File("src/media/malha-exemplo-1.txt");
+        File   arquivo = new File("media/malha-exemplo-1.txt");
         Leitor leitor  = new Leitor(arquivo);
         int[][] matriz = leitor.getMatriz();
         
