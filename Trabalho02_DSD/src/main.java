fc2e0b6df735c32db73a5b3c361b96e29936647d
LucaseNogueira <lucasenogueira@outlook.com>
2022-05-22 23:22:01 -0300

    Atualização na classe Main
    1. - Delegado a escolha ao usuário do arquivo utilizado para a malha viária;

--- Trabalho02_DSD/src/main.java
+++ Trabalho02_DSD/src/main.java
@@ -1,5 +1,7 @@
 
 import java.io.File;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import utils.Leitor;
 
 /**
@@ -8,19 +10,85 @@ import utils.Leitor;
  */
 public class main {
     
+    private static JFrame frame;
+    public static final String NOME_TRABALHO = "Trabalho02_DSD";
+    
     public static void main(String[] args){
-        File   arquivo = new File("media/malha-exemplo-1.txt");
-        Leitor leitor  = new Leitor(arquivo);
-        int[][] matriz = leitor.getMatriz();
+        frame                   = new JFrame("JOptionPane");
+        int    tipoBuscaArquivo = escolheTipoBuscaArquivo();
+        String caminhoArquivo   = escolheCaminhoArquivo(tipoBuscaArquivo);
         
-        System.out.println("Quantidade de Linhas: " + leitor.getComprimento());
-        System.out.println("Quantidade de Colunas: " + leitor.getLargura());
-        for (int i = 0; i < leitor.getComprimento(); i++) {
-            for (int j = 0; j < leitor.getLargura(); j++) {
-                System.out.print(matriz[i][j] + " ");
-            }
-            System.out.println("");
+        System.exit(0);
+    }
+    
+    private static int escolheTipoBuscaArquivo(){
+        String[] tipoBuscaArquivo  = new String[2];
+        tipoBuscaArquivo[0]        = "Caminho do Arquivo";//Busca pelo caminho do arquivo;
+        tipoBuscaArquivo[1]        = "Opções Padrões";    //Escolhe uma das opções padrões;
+        
+        int opção = JOptionPane.showOptionDialog(
+                        frame,
+                        "Escolha um Tipo de Busca de Arquivo",
+                        NOME_TRABALHO,
+                        JOptionPane.DEFAULT_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        tipoBuscaArquivo,
+                        null);
+        
+        return opção;
+    }
+    
+    private static String escolheCaminhoArquivo(int tipoBuscaArquivo){
+        String caminhoArquivo = "";
+        switch(tipoBuscaArquivo){
+            case 0:
+                caminhoArquivo = informaCaminhoArquivo();
+                break;
+            case 1:
+                caminhoArquivo = informaCaminhoPadrao();
+                break;
         }
+        
+        return caminhoArquivo;
+    }
+    
+    private static String informaCaminhoArquivo(){
+        String caminho = JOptionPane.showInputDialog(frame, "Insira o Caminho do Arquivo!", NOME_TRABALHO, 0);
+        
+        return caminho;
+    }
+    
+    private static String informaCaminhoPadrao(){
+        String[] opcao  = new String[3];
+        opcao[0]        = "Exemplo 1";
+        opcao[1]        = "Exemplo 2";
+        opcao[2]        = "Exemplo 3";
+        
+        int opção = JOptionPane.showOptionDialog(
+                        frame,
+                        "Escolha um Tipo de Busca de Arquivo",
+                        NOME_TRABALHO,
+                        JOptionPane.DEFAULT_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        opcao,
+                        null);
+        
+        String caminho = "";
+        switch(opção){
+            case 0:
+                caminho = "src/malha-exemplo-1.txt";
+                break;
+            case 1:
+                caminho = "src/malha-exemplo-2.txt";
+                break;
+            case 2:
+                caminho = "src/malha-exemplo-3.txt";
+                break;
+        }
+        
+        return caminho;
     }
     
 }
