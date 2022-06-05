8affdcc7cd3a3367070e0a1a587dcdb29cf2888a
Fabio <nabata99@gmail.com>
2022-06-05 17:29:11 -0300

    Ajustado imagens da malha na interface visual

--- Trabalho02_DSD/src/utils/ImagemUtil.java
+++ Trabalho02_DSD/src/utils/ImagemUtil.java
@@ -6,74 +6,105 @@ package utils;
  * @author Fabio Frare
  */
 public class ImagemUtil {
-    
+
     public static final String ARQUIVO_CAMINHO_PADRAO = "media/icone/";
-    public static final String ARQUIVO_EXTENCAO       = ".png";
-    
-    public static String getCaminhoImageIcon(String nomeObjeto){
+    public static final String ARQUIVO_EXTENCAO = ".png";
+
+    public static String getCaminhoImageIcon(String nomeObjeto) {
         return ARQUIVO_CAMINHO_PADRAO + nomeObjeto + ARQUIVO_EXTENCAO;
     }
-    
-    public static String getCaminhoImageIcon(int sentidoVia){
+
+    public static String getCaminhoImageIcon(int sentidoVia) {
         String nomeObjeto = getSentencaSentidoObjeto(sentidoVia);
-        
+
         return ARQUIVO_CAMINHO_PADRAO + nomeObjeto + ARQUIVO_EXTENCAO;
     }
-    
-    public static String getCaminhoImageIcon(String nomeVeiculo, int sentidoVeiculo, int sentidoVia){
+
+    public static String getCaminhoImageIcon(String nomeVeiculo, int sentidoVeiculo, int sentidoVia) {
         String nomeObjeto = getSentencaSentidoObjeto(sentidoVia);
         int sentidoTratado = getSentidoVeiculoTratado(sentidoVeiculo);
-        
+
         return ARQUIVO_CAMINHO_PADRAO + nomeVeiculo + "_" + nomeObjeto + "_" + sentidoTratado + ARQUIVO_EXTENCAO;
     }
-    
-    public static String getNomePadraoPosicaoMatriz(int posicao){
+
+    public static String getNomePadraoPosicaoMatriz(int posicao) {
         String nomeObjeto = "";
-        
-        if(posicao == 5 || posicao == 6 || posicao == 7 || posicao == 8 || posicao == 9 || posicao == 10 || posicao == 11 || posicao == 12){
-            nomeObjeto = "cruzamento";
+
+        switch (posicao) {
+            case 0:
+                nomeObjeto = "grama";
+                break;
+            case 5:
+                nomeObjeto = "cruzamento";
+                break;
+            case 6:
+                nomeObjeto = "cruzamento";
+                break;
+            case 7:
+                nomeObjeto = "cruzamento";
+                break;
+            case 8:
+                nomeObjeto = "cruzamento";
+                break;
+            case 9:
+                nomeObjeto = "cruzamento";
+                break;
+            case 10:
+                nomeObjeto = "cruzamento";
+                break;
+            case 11:
+                nomeObjeto = "cruzamento";
+                break;
+            case 12:
+                nomeObjeto = "cruzamento";
+                break;
+            default:
+                System.out.println("Classe: ImagemUtil no método getNomePadraoPosicaoMatriz()");
+
         }
-        else
-        if(posicao == 0){
-            nomeObjeto = "grama";
-        }
-        
+
+//        if(posicao == 5 || posicao == 6 || posicao == 7 || posicao == 8 || posicao == 9 || posicao == 10 || posicao == 11 || posicao == 12){
+//            nomeObjeto = "cruzamento";
+//        }
+//        else
+//        if(posicao == 0){
+//            nomeObjeto = "grama";
+//        }
         return nomeObjeto;
     }
-    
-    private static String getSentencaSentidoObjeto(int sentidoObjeto){
-        String resposta        = "";
+
+    private static String getSentencaSentidoObjeto(int sentidoObjeto) {
+        String resposta = "";
         String respostaDirecao = "";
-        
-        if(sentidoObjeto == 1 || sentidoObjeto == 3){
+
+        if (sentidoObjeto == 1 || sentidoObjeto == 3) {
             respostaDirecao = "_esquerda";
-            if(sentidoObjeto == 3){
+            if (sentidoObjeto == 3) {
                 respostaDirecao = "_direita";
             }
             resposta = "estrada_vertical" + respostaDirecao;
         }
-        if(sentidoObjeto == 2 || sentidoObjeto == 4){
+        if (sentidoObjeto == 2 || sentidoObjeto == 4) {
             respostaDirecao = "_baixo";
-            if(sentidoObjeto == 4){
+            if (sentidoObjeto == 4) {
                 respostaDirecao = "_cima";
             }
             resposta = "estrada_horizontal" + respostaDirecao;
         }
-        
+
         return resposta;
     }
-    
-    private static int getSentidoVeiculoTratado(int sentidoVeiculo){
+
+    private static int getSentidoVeiculoTratado(int sentidoVeiculo) {
         int i;
-        
-        if(sentidoVeiculo == 1 || sentidoVeiculo == 2){
+
+        if (sentidoVeiculo == 1 || sentidoVeiculo == 2) {
             i = 1;
-        }
-        else{
+        } else {
             i = 2;
         }
-        
+
         return i;
     }
-    
+
 }
